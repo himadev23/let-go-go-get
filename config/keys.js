ceeb1e6464c21ddf79852ef8df21034d2647483b
himaja gattu <himajagattu@himajas-MBP.hsd1.ca.comcast.net>
2018-03-07 20:01:06 -0800

    adding files

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '970822402174-kas9ufk99scgv75orrqd4n4lb3eaprq4.apps.googleusercontent.com',
+  googleClientSecretKey: 'VtkJ9yb0cQp3XWJX8H9g-hOP',
+  
+  cookieKey: 'hjkhjsjfdjkshfjdsjkfjkdsfjksdjfjs'
+};
