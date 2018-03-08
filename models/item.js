f0e71f2fb2766d80f7c5428b11a6ae213afdaa76
Rhonda Glennon <rhondaglennon@gmail.com>
2018-03-08 11:13:03 -0800

    clean up comments and update indentation

--- models/item.js
+++ models/item.js
@@ -2,27 +2,25 @@
 
 module.exports = function(sequelize, DataTypes) {
   var Item = sequelize.define("Item", {
-      category: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      description: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      photo_url {
-        type: DataTypes.STRING
-      },
-      user_email {
-        type: DataTypes.STRING,
-        allowNull: false,
-        primaryKey: true
-      }
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    photo_url {
+      type: DataTypes.STRING
+    },
+    user_email {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true
+    }
   });
   
   Item.associate = function(models) {
-    // We're saying that a Post should belong to an Author
-    // A Post can't be created without an Author due to the foreign key constraint
     Item.belongsTo(models.User, {
       foreignKey: {
         allowNull: false
--- models/user.js
+++ models/user.js
@@ -2,22 +2,20 @@
 
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        primaryKey: true,
-        validate: {
-          isEmail: true
-        }
-      },
-      contact_info: {
-        type: DataTypes.STRING
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      validate: {
+        isEmail: true
       }
+    },
+    contact_info: {
+      type: DataTypes.STRING
+    }
   });
   
   User.associate = function(models) {
-    // Associating Author with Posts
-    // When an Author is deleted, also delete any associated Posts
     User.hasMany(models.Item, {
       onDelete: "cascade"
     });
