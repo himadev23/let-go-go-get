2fb38b78834a2a06566282fc0486ae2c4006e678
John Cordano <jecordano@yahoo.com>
2018-03-10 08:22:04 -0800

    Changed names of .css files. Changed paths to .css files in html files.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -41,4 +41,13 @@ $(function() {
       }
     );
   });
+
+
+
+$(document).ready(function() {
+  $("#item-name").text("Item name");
+  $("#item-category").text("Item category");
+  $("#item-description").text("Item Description");
+  $("#item-photo").text("Item Photo");
+  });
 });
---
+++ public/assets/js/goget.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+$(document).ready(function() {
+  $("#item-name").text("Item name");
+  $("#item-category").text("Item category");
+  $("#item-description").text("Item Description");
+  $("#item-photo").text("Item Photo");
+  });
+});
