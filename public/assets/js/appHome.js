d784eeb65df390b1be1d27838f64dd1992b6811c
Rhonda Glennon <rhondaglennon@gmail.com>
2018-03-11 22:33:19 -0700

    clean up code and add comments

--- public/assets/js/appHome.js
+++ public/assets/js/appHome.js
@@ -1,18 +1,19 @@
-// Wait until the DOM is fully loaded to attach the handlers.
 $(document).ready(function() {
-  
+
+  // build the initial page from the total number of items in the database (or the first 10, if there are more)
+  // create the page using a masonry.js templating style
   $.ajax("api/items", {
     type: "GET"
   }).then(
     function(res) {
-      for (var i = 0; ((i < res.length) || (i < 4)); i++) {
+      
+      // loop through the response and display the first 10 items returned (or the total number of responses if fewer are returned)
+      
+      for (var i = 0; (i < res.length) && (i < 10); i++) {
         var itemTag = $("<a>");
         itemTag.addClass("stuff");
-        //itemTag.data("target", "#item-modal");
-        //  itemTag.data("toggle", "modal");
         //itemTag.attr("href", "#item-modal");
         itemTag.attr("id", res[i].id);
-        // itemTag.attr("data-content", "hello");
         itemTag.attr("title", res[i].name);
         var article = $("<article>");
         var figure = $("<figure>");
@@ -27,41 +28,22 @@ $(document).ready(function() {
         $("#main-grid").append(itemTag);
       }
     });
-    
-    // use event delegation to capture the click of the a tag in the main grid because content is generated dynamically
-    $("#main-grid").on("click", "a", function(event) {
-      
-      var id = $(this).attr("id");
-      $.ajax({
-        type: "GET",
-        url: "/api/items/" + id
-      }).done(
-        function(res) {
-          console.log(res.name);
-          $("#item-modal").modal("toggle");
-          $(".modal-header").text(res.name);
-          $("#item-description").text(res.description);
-          $("#item-category").text(res.category);
-          //var photo = $("<img>");
-          $("#item-image").attr("src", res.photo_url);
-          // needs to be img-responsive to make it resize to fit within the modal window
-        //  photo.addClass("img-responsive");
-        //  $(".modal-body").append(photo);
-        }
-      );
-    }); 
 
-
-    // var originalModal = $('#item-modal').clone();
-    // $(document).on('#item-modal', 'hidden.bs.modal', function () {
-    //     $('#item-modal').remove();
-    //     var myClone = originalModal.clone();
-    //     $('body').append(myClone);
-    // });
-
-    
+  // use event delegation to capture the click of the a tag in the main grid because content is generated dynamically and show the modal with the content
+  $("#main-grid").on("click", "a", function(event) {
+    var id = $(this).attr("id");
+    $.ajax({
+      type: "GET",
+      url: "/api/items/" + id
+    }).done(
+      function(res) {
+        $("#item-modal").modal("toggle");
+        $(".modal-header").text(res.name);
+        $("#item-description").text(res.description);
+        $("#item-category").text(res.category);
+        $("#item-image").attr("src", res.photo_url);
+      }
+    );
   });
-    
 
-
-
+});
