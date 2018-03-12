0e502f79b6ba58cc009c41c7d36674afee8fa7b6
Rhonda Glennon <rhondaglennon@gmail.com>
2018-03-11 22:36:08 -0700

    delete unused item details html route

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -7,11 +7,6 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
   
-  // display a page with one item's details - note: adding /details because : it fails to display items/new otherwise
-  app.get("/items/details/:id", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/goGet.html"));
-  });
-  
   // display page to add a new item (show a form)
   app.get("/items/new", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/letGo.html"));
