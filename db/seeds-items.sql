6022af81a97653ef33ed5bfd2cec416ed1d8baff
Rhonda Glennon <rhondaglennon@gmail.com>
2018-03-14 19:16:01 -0700

    delete unneeded seeds files

--- db/seeds-items.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO items (category, description, photo_url, user_email) VALUES ("furniture", "IKEA bed", "http://www.ikea.com/us/en/images/products/kopardal-bed-frame-gray__0418675_PE575712_S4.JPG", "hello@a.com");
-
-INSERT INTO items (category, description, photo_url, user_email) VALUES ("electronics", "iPhone X", "https://www.att.com/catalog/en/skus/Apple/Apple%20iPhone%20X/overview/326743-pdp-iphoneX-s2-1@2x.jpg", "hello@a.com");
-
-INSERT INTO items (category, description, photo_url, user_email) VALUES ("clothes", "pink t-shirt", "https://assets.teenvogue.com/photos/5aa003161710f94783e9a03d/3:2/w_757/vegan%2520fb.jpg", "hola@b.com");
-
-INSERT INTO items (category, description, photo_url, user_email) VALUES ("appliances", "fancy fridge", "http://www.ikea.com/us/en/images/products/nutid-side-by-side-refrigerator__0324893_PE517257_S4.JPG", "hi@c.com");
